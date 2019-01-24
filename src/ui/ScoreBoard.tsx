4275ec84c6db15b887563ca11b409d60518fbea9
Luis Rodrigues <goblindegook@users.noreply.github.com>
2019-01-24 01:09:02 +0000

    Scoreboard style tweaks

--- src/ui/ScoreBoard.tsx
+++ src/ui/ScoreBoard.tsx
@@ -4,24 +4,24 @@ import { Score } from '../engine/scores'
 
 const Board = styled('table')`
   border: 1px solid white;
-  width: 20vw;
+  min-width: 25vw;
   padding: 1rem;
 `
 
 const PlayerHeader = styled('th')`
   color: white;
   padding: 1rem;
-`
-
-const RowHeader = styled('td')`
-  color: white;
-  padding: 1rem;
+  text-align: center;
 `
 
 const Cell = styled('td')`
   color: white;
   padding: 1rem;
-  text-align: right;
+  text-align: center;
+`
+
+const RowHeader = styled(Cell)`
+  text-align: left;
 `
 
 type ScoreBoardProps = {
