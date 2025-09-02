9b5c3b31124b55f430b5a92e513f670ebd6652af
Luis Rodrigues <lrodrigues@newstore.com>
2025-09-02 17:50:06 +0200

    Update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { shuffle } from '@pacote/shuffle'
 import { deck } from './engine/cards'
-import { findMatches } from './engine/match'
 import { deal, play } from './engine/scopa'
 import { score } from './engine/scores'
 import { Game } from './ui/Game'
+import { move } from './engine/opponent-basic'
 
 const dealShuffledDeck = () => deal(shuffle(deck()), { players: 2 })
 
@@ -11,16 +11,7 @@ const App = () => (
   <Game
     onStart={dealShuffledDeck}
     onPlay={play}
-    onOpponentTurn={async (game) => {
-      // SPIKE
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      const card = game.players[game.turn].hand[0]
-      const available = findMatches(card[0], game.table)
-      const mustPick = Math.min(...available.map((t) => t.length))
-      const valid = available.filter((t) => t.length === mustPick)
-      const targets = valid[0] || []
-      return { card, targets }
-    }}
+    onOpponentTurn={move}
     onScore={score}
   />
 )
