290c24e5f4092cab6b93591e6702d04e65397b88
Luis Rodrigues <lrodrigues@newstore.com>
2026-08-07 19:12:42 +0200

    chore: react plugin without swc

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
